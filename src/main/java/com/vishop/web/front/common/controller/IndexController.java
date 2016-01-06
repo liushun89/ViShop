99bc95435a9a408c7213ad3a6389026d2b06dd52
homiss <hou_sz@qq.com>
2016-01-06 22:17:21 +0800

    first commit

---
+++ src/main/java/com/vishop/web/front/common/controller/IndexController.java
@@ -0,0 +1,22 @@
+package com.vishop.web.front.common.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * IndexController
+ *
+ * @author Homiss
+ * @date 2016/1/6
+ */
+@Controller
+@RequestMapping("/")
+public class IndexController {
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+
+}
